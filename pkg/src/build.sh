4ee588c9ec20c6da9d56a7379e5499e8044d6056
Art Poon <artpoon@gmail.com>
2016-10-20 23:11:31 -0400

    dropping custom igraph; see issue #3

--- pkg/src/build.sh
+++ pkg/src/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-gcc -c test.c -I/usr/local/include/igraph -I/Library/Frameworks/R.framework/Versions/3.3/Resources/include
+gcc -c test.c -I/usr/local/include/igraph -I/Library/Frameworks/R.framework/Versions/3.2/Resources/include
 
