6f8cc7bd3f20b9ad5663478ec94a6b923b002a73
Art Poon <artpoon@gmail.com>
2016-12-16 15:45:23 -0500

    forgot to add source files to commit

--- pkg/R/smcABC.R
+++ pkg/R/smcABC.R
@@ -1,3 +1,19 @@
+# This file is part of Kaphi.
+
+# Kaphi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Kaphi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Kaphi.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # Based on adaptive SMC algorithm proposed by Del Moral, Pierre, Arnaud
 # Doucet, and Ajay Jasra. "An adaptive sequential Monte Carlo method for
 # approximate Bayesian computation." Statistics and Computing 22.5 (2012):
--- pkg/src/kernel.c
+++ pkg/src/kernel.c
@@ -1,4 +1,19 @@
-/* modeled on rigraph/tools/stimulus/rinterface_extra.c */
+/*
+ *  This file is part of Kaphi.
+ *
+ *  Kaphi is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Kaphi is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kaphi.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <igraph.h>
 #include <igraph_error.h>
@@ -14,8 +29,6 @@
 #include "treestats.h"
 
 void yy_scan_string(const char *);
-
-
 
 
 igraph_t * R_Kaphi_parse_newick(SEXP newick) {
