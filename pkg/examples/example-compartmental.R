c496abe0683fd94a8e8187ffee8ec9af2dae3025
Tammy Ng <tng92@uwo.ca>
2017-06-23 17:56:25 -0400

    Updated examples in Rd files issue #38 --Tammy Ng

--- pkg/examples/example-compartmental.R
+++ pkg/examples/example-compartmental.R
@@ -9,7 +9,7 @@ config$nsample <- 5
 
 # simulate target tree
 #theta <- c(t.end=30.*52, N=1000, beta=0.01, gamma=1/520, mu=1/3640, alpha=0)
-theta <- c(t.end=1000, N=1000, beta=0.01, gamma=1/520, mu=1/3640, alpha=0)
+theta <- c(t.end=1000, N=500, beta=0.01, gamma=1/520, mu=1/3640, alpha=0)
 set.seed(7)
 obs.tree <- compartmental.model(theta, nsim=1, tips=100, model='sir.nondynamic')[[1]]
 obs.tree <- parse.input.tree(obs.tree, config)
