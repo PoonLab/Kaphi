cf664c4c22f3ee93ef720ce811649ad42bd2b209
Mathias <mathias.renaud5@gmail.com>
2017-07-04 17:06:53 -0400

    scripts for yule model example runs

--- pkg/examples/example-yule.R
+++ pkg/examples/example-yule.R
@@ -29,7 +29,7 @@ res <- sapply(x, function(val) {
 
 # generate a plot
 par(mar=c(5,5,2,2))
-plot(x, res, type='o', xlab='Lambda', ylab='Mean kernel distance', cex.lab=1.2, ylim=c(0,0.2), 
+plot(x, res, type='o', xlab='Lambda', ylab='Mean kernel distance', cex.lab=1.2, ylim=c(0,0.6), 
      main='Identifiability of Lambda (Yule Model) - Normalized')
 abline(v=0.1, lty=2)
 
@@ -49,33 +49,38 @@ trace <- read.table('pkg/examples/example-yule2.tsv', header=T, sep='\t')
 par(mar=c(5,5,2,2))
 plot(
   sapply(split(trace$lambda*trace$weight, trace$n), sum), 
-  ylim=c(0, 0.8), 
-  type='b',
+  ylim=c(0, 1.5), 
+  type='o',
   xlab='Iteration', 
   ylab='Mean lambda',
-  cex.lab=1.2
+  cex.lab=1,
+  main='Trajectory of Mean Lambda (Yule Model, 100 particles)'
 )
 abline(h=0.09, lty=2)
 abline(h=0.11, lty=2)
 
 # use kernel densities to visualize posterior approximations
-pal <- rainbow(n=6, start=0, end=0.3, v=0.8, s=0.5)
+pal <- rainbow(n=6, start=0, end=0.5, v=1, s=1)
 par(mar=c(5,5,2,2))
-plot(density(trace$lambda[trace$n==1], weights=trace$weight[trace$n==1]), xlim=c(-0.5, 2), col=pal[1], lwd=2, main=NA, xlab='Yule rate parameter (lambda)', cex.lab=1.2, ylim=c(0, 10))
-for (i in 1:8) {
-  temp <- trace[trace$n==i*2,]
+plot(density(trace$lambda[trace$n==1], weights=trace$weight[trace$n==1]), xlim=c(0, 2), col=pal[1], lwd=2, main='Yule (gamma: shape=2, rate=1)', xlab='Yule rate parameter (lambda)', cex.lab=1.2, ylim=c(0, 10))
+
+for (i in 1:6) {
+  temp <- trace[trace$n==i*10,]
   lines(density(temp$lambda, weights=temp$weight), col=pal[i+1], lwd=1.5)
+  #cat('iter:', i*10, '\n')
+  #Sys.sleep(2)
 }
-lines(density(trace$lambda[trace$n==max(trace$n)], weights=trace$weight[trace$n==max(trace$n)]), col=pal[length(pal)], lwd=2)  # final estimates
+lines(density(trace$lambda[trace$n==max(trace$n)], weights=trace$weight[trace$n==max(trace$n)]), col='black', lwd=2)  # final estimates
+abline(v=0.1, lty=3, col='red')
 
 # show the prior distribution
 x <- seq(0, 2, 0.01)
 y <- function(x) {arg.prior <- x; eval(parse(text=config$prior.densities[["lambda"]]))}
-lines(x, y(x), lty=2)
+lines(x, y(x), lty=5)
 
 # show posterior distribution (work in progress)
 node.heights <- rev(branching.times(obs.tree))
 
 # make a legend
-legend(x=2300, y=0.0025, legend=c('prior', 't=1', 't=10', 't=20', 't=30', 't=40', 't=47'), lty=c(2,rep(1,6)), col=c('black', pal), lwd=c(1,2,rep(1.5,4),2), seg.len=3)
+legend(x=0.98, y=10.1, legend=c('prior', 'n=10', 'n=20', 'n=30', 'n=40', 'n=50', 'n=60', 'n=68(final)', 'true lambda(0.1)'), lty=c(5,rep(1,7),3), col=c('black', pal, 'black', 'red'), lwd=c(1,2,rep(1.5,5),2,0.75), seg.len=2)
 
