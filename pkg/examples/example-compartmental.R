4d37ca4b08c98ca6cd42716a8f9a4ffe7d010401
Tammy Ng <tng92@uwo.ca>
2017-08-17 10:49:45 -0400

    Working on issue#93 and #42 --Tammy Ng

--- pkg/examples/example-compartmental.R
+++ pkg/examples/example-compartmental.R
@@ -6,7 +6,7 @@ config <- set.model(config, 'sir.dynamic')
 
 # simulate target tree
 theta <- c(t.end=200, N=10000, beta=0.1, gamma=0.002, mu=0.0001, alpha=5)
-set.seed(40)
+set.seed(50)
 obs.tree <- compartmental.model(theta, nsim=1, tips=100, model='sir.dynamic', fgyResolution=1000)[[1]]
 obs.tree <- parse.input.tree(obs.tree, config)
 
@@ -20,6 +20,9 @@ result <- run.smc(ws, trace.file='pkg/examples/example-compartmental.tsv', model
 
 # let's examine the contents of the trace file
 trace <- read.table('pkg/examples/example-compartmental.tsv', header=T, sep='\t')
+
+########################################################################################################################################################################
+pdf(file='pkg/examples/example-comparmental.pdf')
 
 # trajectory of mean estimate of beta
 par(mar=c(5,5,2,2))
@@ -43,7 +46,7 @@ plot(density
      weights=trace$weight[trace$n==1]), 
      col=pal[1], 
      lwd=2, 
-     main='SIRD (gamma: shape=1, rate=5)', 
+     main='SIRD (gamma: shape=1, rate=10)', 
      xlab='SIRD rate parameter (beta)', 
      cex.lab=1.2
      )
@@ -72,7 +75,7 @@ node.heights <- rev(branching.times(obs.tree))
 # make a legend
 legend(x=1, y=10, legend=c('prior', 'n=1', 'n=10', 'n=20', 'n=30', 'n=40', 'n=50', 'n=60', 'n=70','n=71(final)', 'true beta(0.1)'), lty=c(5,rep(1,9),3), col=c('black', pal, 'black', 'red'), lwd=c(1,2,rep(1.5,7),2,0.75), seg.len=2)
 
-
+######################################################################################################################################################################
 
 # trajectory of mean estimate of gamma
 par(mar=c(5,5,2,2))
@@ -82,10 +85,51 @@ plot(
   xlab='Iteration', 
   ylab='Mean gamma',
   cex.lab=1,
-  main='Trajectory of Mean Gamma (SIRD Model, 20 particles)'
+  main='Trajectory of Mean Gamma (SIRD Model, 500 particles)'
 )
-abline(h=1/520, lty=2)
+abline(h=0.002, lty=2)
 
+# use kernel densities to visualize posterior approximations
+pal <- rainbow(n=8, start=0, end=0.5, v=1, s=1)
+par(mar=c(5,5,2,2))
+plot(density
+     (trace$gamma[trace$n==1], 
+       weights=trace$weight[trace$n==1]), 
+     col=pal[1], 
+     lwd=2, 
+     main='SIRD (gamma: shape=1, rate=10)', 
+     xlab='SIRD rate parameter (gamma)', 
+     cex.lab=1.2
+)
+
+for (i in 1:7) {
+  temp <- trace[trace$n==i*10,]
+  lines(density(temp$gamma, weights=temp$weight), col=pal[i+1], lwd=1.5)
+}
+lines(density
+      (trace$gamma[trace$n==max(trace$n)], 
+        weights=trace$weight[trace$n==max(trace$n)]), 
+      col='black', 
+      lwd=2
+)  # final estimates
+abline(v=0.002, lty=3, col='red')
+
+
+# show the prior distribution
+x <- seq(0, 2, 0.01)
+y <- function(x) {arg.prior <- x; eval(parse(text=config$prior.densities[["gamma"]]))}
+lines(x, y(x), lty=5)
+
+# show posterior distribution (work in progress)
+node.heights <- rev(branching.times(obs.tree))
+
+# make a legend
+legend(x=1, y=10, legend=c('prior', 'n=1', 'n=10', 'n=20', 'n=30', 'n=40', 'n=50', 'n=60', 'n=70','n=71(final)', 'true beta(0.1)'), lty=c(5,rep(1,9),3), col=c('black', pal, 'black', 'red'), lwd=c(1,2,rep(1.5,7),2,0.75), seg.len=2)
+
+
+
+
+########################################################################################################################################################################
 # trajectory of mean estimate of mu
 par(mar=c(5,5,2,2))
 plot(
@@ -94,9 +138,151 @@ plot(
   xlab='Iteration', 
   ylab='Mean mu',
   cex.lab=1,
-  main='Trajectory of Mean Mu (SIRD Model, 20 particles)'
+  main='Trajectory of Mean Mu (SIRD Model, 500 particles)'
 )
-abline(h=1/3640, lty=2)
+abline(h=0.0001, lty=2)
+
+# use kernel densities to visualize posterior approximations
+pal <- rainbow(n=8, start=0, end=0.5, v=1, s=1)
+par(mar=c(5,5,2,2))
+plot(density
+     (trace$mu[trace$n==1], 
+       weights=trace$weight[trace$n==1]), 
+     col=pal[1], 
+     lwd=2, 
+     main='SIRD (gamma: shape=1, rate=10)', 
+     xlab='SIRD rate parameter (mu)', 
+     cex.lab=1.2
+)
+
+for (i in 1:7) {
+  temp <- trace[trace$n==i*10,]
+  lines(density(temp$mu, weights=temp$weight), col=pal[i+1], lwd=1.5)
+}
+lines(density
+      (trace$mu[trace$n==max(trace$n)], 
+        weights=trace$weight[trace$n==max(trace$n)]), 
+      col='black', 
+      lwd=2
+)  # final estimates
+abline(v=0.0001, lty=3, col='red')
+
+
+# show the prior distribution
+x <- seq(0, 2, 0.01)
+y <- function(x) {arg.prior <- x; eval(parse(text=config$prior.densities[["mu"]]))}
+lines(x, y(x), lty=5)
+
+# show posterior distribution (work in progress)
+node.heights <- rev(branching.times(obs.tree))
+
+# make a legend
+legend(x=1, y=10, legend=c('prior', 'n=1', 'n=10', 'n=20', 'n=30', 'n=40', 'n=50', 'n=60', 'n=70','n=71(final)', 'true beta(0.1)'), lty=c(5,rep(1,9),3), col=c('black', pal, 'black', 'red'), lwd=c(1,2,rep(1.5,7),2,0.75), seg.len=2)
 
 
 
+#########################################################################################################################################################################
+# trajectory of mean estimate of t.end
+par(mar=c(5,5,2,2))
+plot(
+  sapply(split(trace$t.end*trace$weight, trace$n), sum), 
+  type='o',
+  xlab='Iteration', 
+  ylab='Mean t.end',
+  cex.lab=1,
+  main='Trajectory of Mean t.end (SIRD Model, 500 particles)'
+)
+abline(h=200, lty=2)
+
+
+# use kernel densities to visualize posterior approximations
+pal <- rainbow(n=8, start=0, end=0.5, v=1, s=1)
+par(mar=c(5,5,2,2))
+plot(density
+     (trace$t.end[trace$n==1], 
+       weights=trace$weight[trace$n==1]), 
+     col=pal[1], 
+     lwd=2, 
+     main='SIRD (normal: mean=200, sd=50)', 
+     xlab='SIRD rate parameter (t.end)', 
+     cex.lab=1.2
+)
+
+for (i in 1:7) {
+  temp <- trace[trace$n==i*10,]
+  lines(density(temp$t.end, weights=temp$weight), col=pal[i+1], lwd=1.5)
+}
+lines(density
+      (trace$t.end[trace$n==max(trace$n)], 
+        weights=trace$weight[trace$n==max(trace$n)]), 
+      col='black', 
+      lwd=2
+)  # final estimates
+abline(v=200, lty=3, col='red')
+
+
+# show the prior distribution
+x <- seq(0, 2, 0.01)
+y <- function(x) {arg.prior <- x; eval(parse(text=config$prior.densities[["t.end"]]))}
+lines(x, y(x), lty=5)
+
+# show posterior distribution (work in progress)
+node.heights <- rev(branching.times(obs.tree))
+
+# make a legend
+legend(x=1, y=10, legend=c('prior', 'n=1', 'n=10', 'n=20', 'n=30', 'n=40', 'n=50', 'n=60', 'n=70','n=71(final)', 'true beta(0.1)'), lty=c(5,rep(1,9),3), col=c('black', pal, 'black', 'red'), lwd=c(1,2,rep(1.5,7),2,0.75), seg.len=2)
+
+
+#####################################################################################################################################################################
+# trajectory of mean estimate of N
+par(mar=c(5,5,2,2))
+plot(
+  sapply(split(trace$N*trace$weight, trace$n), sum), 
+  type='o',
+  xlab='Iteration', 
+  ylab='Mean N',
+  cex.lab=1,
+  main='Trajectory of Mean N (SIRD Model, 500 particles)'
+)
+abline(h=10000, lty=2)
+
+
+# use kernel densities to visualize posterior approximations
+pal <- rainbow(n=8, start=0, end=0.5, v=1, s=1)
+par(mar=c(5,5,2,2))
+plot(density
+     (trace$N[trace$n==1], 
+       weights=trace$weight[trace$n==1]), 
+     col=pal[1], 
+     lwd=2, 
+     main='SIRD (norm: mean=10000, sd=200)', 
+     xlab='SIRD rate parameter (N)', 
+     cex.lab=1.2
+)
+
+for (i in 1:7) {
+  temp <- trace[trace$n==i*10,]
+  lines(density(temp$N, weights=temp$weight), col=pal[i+1], lwd=1.5)
+}
+lines(density
+      (trace$N[trace$n==max(trace$n)], 
+        weights=trace$weight[trace$n==max(trace$n)]), 
+      col='black', 
+      lwd=2
+)  # final estimates
+abline(v=10000, lty=3, col='red')
+
+
+# show the prior distribution
+x <- seq(0, 2, 0.01)
+y <- function(x) {arg.prior <- x; eval(parse(text=config$prior.densities[["N"]]))}
+lines(x, y(x), lty=5)
+
+# show posterior distribution (work in progress)
+node.heights <- rev(branching.times(obs.tree))
+
+# make a legend
+legend(x=1, y=10, legend=c('prior', 'n=1', 'n=10', 'n=20', 'n=30', 'n=40', 'n=50', 'n=60', 'n=70','n=71(final)', 'true beta(0.1)'), lty=c(5,rep(1,9),3), col=c('black', pal, 'black', 'red'), lwd=c(1,2,rep(1.5,7),2,0.75), seg.len=2)
+###########################################################################################################################################################
+
+dev.off()
