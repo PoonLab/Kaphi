350b7dd75d64eec99960fb5b548429bfbbfcbbee
Tammy Ng <tng92@uwo.ca>
2017-09-08 13:58:45 -0400

    Template creates corrected XML formatted file and calls MASTER, returning a Newick back to the user --Tammy Ng

--- pkg/R/master-xml-template.R
+++ pkg/R/master-xml-template.R
@@ -1,3 +1,4 @@
+setwd('~/git/Kaphi/')
 require(whisker)
 
 # template
@@ -10,44 +11,62 @@ template <-
               <run spec='InheritanceTrajectory'
                    samplePopulationSizes='true'
                    verbosity='2'
-                   simulationTime='10'
+                   simulationTime='{{ tend }}'
                    seed='42'>
             
                 <model spec='Model' id='model'>
-                  <population spec='Population' id='V' populationName='V' />
+                  <population spec='Population' id='S' populationName='S' />
+                  <population spec='Population' id='I' populationName='I' />
+                  <population spec='Population' id='R' populationName='R' />
+                  <population spec='Population' id='I_sample' populationName='I_sample' />
             
-                  <reaction spec='Reaction' reactionName='birth' rate='1.0'>
-                    {{{reaction}}}
+                  <reaction spec='Reaction' reactionName='Infection' rate='{{ beta }}'>
+                    S + I -> 2I
+                  </reaction>
+                  <reaction spec='Reaction' reactionName='Recovery' rate='{{ gamma }}'>
+                    I -> R
+                  </reaction>
+                  <reaction spec='Reaction' reactionName='Sampling' rate='{{ phi }}'>
+                    I -> I_sample
                   </reaction>
                 </model>
             
                 <initialState spec='InitState'>
-                  <lineageSeed spec='Individual' population='@V' />
+                  <populationSize spec='PopulationSize' population='@S' size='{{ N }}'/>
+                  <lineageSeed spec='Individual' population='@I'/>
                 </initialState>
             
-                <inheritancePostProcessor spec='LineageSampler' reverseTime='false' noClean='false' nSamples='10'>
-            <!--
-                  <populationSize spec='PopulationSize' size='10'>
-                    <population spec='Population' populationName='T'/>
-                  </populationSize>
-             -->
-            <!--
-                  <populationSize spec='PopulationSize' size='10'>
-                    <population spec='Population' populationName='V'/>
-                  </populationSize>
-             -->
-                </inheritancePostProcessor>
-            
-                <output spec='NexusOutput' fileName='test.nexus' collapseSingleChildNodes='false' />
-                <output spec='JsonOutput' fileName='test.json' />
+                <output spec='NexusOutput' fileName='test.nexus' collapseSingleChildNodes='false'/>
+                <output spec='JsonOutput' fileName='test.json'/>
+                <output spec='NewickOutput' fileName='test.newick'/>
               </run>
             </beast>"
 
-# hash
-data <- list(reaction = 'V -> V + V')
+# <inheritancePostProcessor spec='LineageSampler' reverseTime='false' noClean='false' nSamples='10'>
+# </inheritancePostProcessor>
+# <inheritancePostProcessor spec="LineageFilter" reactionName="Sampling"/>
+# <postSimCondition spec="LeafCountPostSimCondition" nLeaves="{{ ntips }}" exact="false"/>
+# <lineageEndCondition spec='LineageEndCondition' nLineages='0'/>
 
+
+# hash
+data <- list(tend = '40',
+             beta = '0.001',
+             gamma = '0.2',
+             phi = '0.01',
+             N = (1000-1))
 
 text <- whisker.render(template, data)
-
-setwd('~/git/Kaphi/')
 write(text, file='pkg/R/temp.xml')
+
+
+
+setwd('~/git/MASTER-5.1.1')
+system2('java', args=c('-jar MASTER-5.1.1.jar', '../Kaphi/pkg/R/temp.xml'), stdout=F, stderr=F)
+
+require(rjson)
+df <- fromJSON(file='test.json')
+plot(df$t, df$S, 's', col='green', xlab='Time', ylab='Population size')
+points(df$t, df$I, 's', col='red', xlab='Time', ylab='Population size')
+points(df$t, df$R, 's', col='blue', xlab='Time', ylab='Population size')
+
