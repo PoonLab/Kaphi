4411109d44b28688c06e2f0f4df422a1a0b98ce8
Tammy Ng <tng92@uwo.ca>
2017-09-08 15:07:35 -0400

    Wrote wrapper function to take in parameters for MASTER and embed into template --Tammy Ng

--- pkg/R/master-xml-template.R
+++ pkg/R/master-xml-template.R
@@ -1,69 +1,111 @@
-setwd('~/git/Kaphi/')
-require(whisker)
+# This file is part of Kaphi.
 
-# template
-template <- 
-           "<beast version='2.0' namespace= 'master
-                                             :master.model
-                                             :master.conditions
-                                             :master.outputs
-                                             :master.postprocessors'>
-              <run spec='InheritanceTrajectory'
-                   samplePopulationSizes='true'
-                   verbosity='2'
-                   simulationTime='{{ tend }}'
-                   seed='42'>
-            
-                <model spec='Model' id='model'>
-                  <population spec='Population' id='S' populationName='S' />
-                  <population spec='Population' id='I' populationName='I' />
-                  <population spec='Population' id='R' populationName='R' />
-                  <population spec='Population' id='I_sample' populationName='I_sample' />
-            
-                  <reaction spec='Reaction' reactionName='Infection' rate='{{ beta }}'>
-                    S + I -> 2I
-                  </reaction>
-                  <reaction spec='Reaction' reactionName='Recovery' rate='{{ gamma }}'>
-                    I -> R
-                  </reaction>
-                  <reaction spec='Reaction' reactionName='Sampling' rate='{{ phi }}'>
-                    I -> I_sample
-                  </reaction>
-                </model>
-            
-                <initialState spec='InitState'>
-                  <populationSize spec='PopulationSize' population='@S' size='{{ N }}'/>
-                  <lineageSeed spec='Individual' population='@I'/>
-                </initialState>
-            
-                <output spec='NexusOutput' fileName='test.nexus' collapseSingleChildNodes='false'/>
-                <output spec='JsonOutput' fileName='test.json'/>
-                <output spec='NewickOutput' fileName='test.newick'/>
-              </run>
-            </beast>"
+# Kaphi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-# <inheritancePostProcessor spec='LineageSampler' reverseTime='false' noClean='false' nSamples='10'>
-# </inheritancePostProcessor>
-# <inheritancePostProcessor spec="LineageFilter" reactionName="Sampling"/>
-# <postSimCondition spec="LeafCountPostSimCondition" nLeaves="{{ ntips }}" exact="false"/>
-# <lineageEndCondition spec='LineageEndCondition' nLineages='0'/>
+# Kaphi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Kaphi.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# hash
-data <- list(tend = '40',
-             beta = '0.001',
-             gamma = '0.2',
-             phi = '0.01',
-             N = (1000-1))
-
-text <- whisker.render(template, data)
-write(text, file='pkg/R/temp.xml')
+epidem.model <- function(theta, nsim, tips, model='sir.nondynamic', seed=NA, labels=NA) {
+  theta <- as.list(theta)
+  if (!is.na(seed)) {
+    set.seed(seed)
+  }
+  
+  trees <- replicate(nsim, # num of simulations
+                     .call.master(theta),
+                     simplify=FALSE
+                     )
+  # cast result as a multiPhylo object
+  class(trees) <- "multiPhylo"
+  return(trees)
+}
+attr(compartmental.model, 'name') <- "epidem.model"  # satisfies requirement in smcConfig.R set.model() function
 
 
 
-setwd('~/git/MASTER-5.1.1')
-system2('java', args=c('-jar MASTER-5.1.1.jar', '../Kaphi/pkg/R/temp.xml'), stdout=F, stderr=F)
+.call.master <- function(theta) {
+  setwd('~/git/Kaphi/')
+  require(whisker)
 
+  ## hash
+  data <- list(tend = as.character(theta$t.end),
+               beta = as.character(theta$beta),
+               gamma = as.character(theta$gamma),
+               phi = as.character(0.0002),
+               N = as.character(theta$N - 1))
+  
+  ## XML template
+  template <- 
+             "<beast version='2.0' namespace= 'master
+                                               :master.model
+                                               :master.conditions
+                                               :master.outputs
+                                               :master.postprocessors'>
+                <run spec='InheritanceTrajectory'
+                     samplePopulationSizes='true'
+                     verbosity='2'
+                     simulationTime='{{ tend }}'
+                     seed='42'>
+              
+                  <model spec='Model' id='model'>
+                    <population spec='Population' id='S' populationName='S' />
+                    <population spec='Population' id='I' populationName='I' />
+                    <population spec='Population' id='R' populationName='R' />
+                    <population spec='Population' id='I_sample' populationName='I_sample' />
+              
+                    <reaction spec='Reaction' reactionName='Infection' rate='{{ beta }}'>
+                      S + I -> 2I
+                    </reaction>
+                    <reaction spec='Reaction' reactionName='Recovery' rate='{{ gamma }}'>
+                      I -> R
+                    </reaction>
+                    <reaction spec='Reaction' reactionName='Sampling' rate='{{ phi }}'>
+                      I -> I_sample
+                    </reaction>
+                  </model>
+              
+                  <initialState spec='InitState'>
+                    <populationSize spec='PopulationSize' population='@S' size='{{ N }}'/>
+                    <lineageSeed spec='Individual' population='@I'/>
+                  </initialState>
+              
+                  <output spec='JsonOutput' fileName='temp.json'/>
+                  <output spec='NewickOutput' fileName='temp.newick'/>
+                </run>
+              </beast>"
+  
+  # <inheritancePostProcessor spec='LineageSampler' reverseTime='false' noClean='false' nSamples='10'>
+  # </inheritancePostProcessor>
+  # <inheritancePostProcessor spec="LineageFilter" reactionName="Sampling"/>
+  # <postSimCondition spec="LeafCountPostSimCondition" nLeaves="{{ ntips }}" exact="false"/>
+  # <lineageEndCondition spec='LineageEndCondition' nLineages='0'/>
+  
+  ## generate temporary XML
+  text <- whisker.render(template, data)
+  write(text, file='pkg/R/temp.xml')
+  
+  ## system call to MASTER with temporarily generated XML
+  setwd('~/git/MASTER-5.1.1')
+  system2('java', args=c('-jar MASTER-5.1.1.jar', '../Kaphi/pkg/R/temp.xml'), stdout=F, stderr=F)
+  
+  ## read Newick, reset to Kaphi directory, and send tree back to user
+  tree <- read.tree(file='temp.newick')
+  setwd('~/git/Kaphi/')
+  tree
+}
+
+
+
+## plotting simulation from json file
 require(rjson)
 df <- fromJSON(file='test.json')
 plot(df$t, df$S, 's', col='green', xlab='Time', ylab='Population size')
